44975ba6ee0cd17358bd1ddcaf048d3e5887a30c
Lawrence Law <d201.c317@gmail.com>
2017-03-09 08:04:08 +0000

    Update on making queries

--- controller/syslog.js
+++ controller/syslog.js
@@ -52,27 +52,66 @@ router.get("/all", (req, res, next) => {
 /**
  * GET one record from Syslog Dataset
  * http://stackoverflow.com/questions/14992123/finding-a-mongodb-document-by-objectid-with-mongoose
+ * FIXME
  */
+/*
 router.get("/id/:id", (req, res, next) => {
-    Syslog.findOne(ObjectId(req.params.id), (err, doc) => {
+    Syslog.findOne({ "_id": ObjectId("58c0f71ca891a2000f27c30e") }, (err, doc) => {
         if (err) {
             return next(err);
         }
         res.write(doc);
     });
 });
-
+*/
 
 /**
- * GET records from Syslog Dataset by severity
- * http://stackoverflow.com/questions/14992123/finding-a-mongodb-document-by-objectid-with-mongoose
+ * GET records from Syslog Dataset by tag
+ * http://stackoverflow.com/questions/6043847/how-do-i-query-for-distinct-values-in-mongoose
  */
-router.get("/serverity/:serverity", (req, res, next) => {
+router.get("/tags", (req, res, next) => {
     let index = 0;
     res.setHeader("Content-Type", "application/json; charset=utf-8");
-    let stream = Syslog.find({
-        serverity: req.params.serverity
-    }, null, {
+    let stream = Syslog.distinct("tag").lean().cursor();
+    res.write("[");
+    stream.on("data", (doc) => {
+        res.write((!(index++) ? "" : ",") + JSON.stringify(doc));
+    });
+    stream.on("close", () => {
+        res.write("]");
+        res.end();
+    });
+    stream.on("error", (err) => {
+        return next(err);
+    });
+});
+
+router.get("/tag/:tag", (req, res, next) => {
+    let index = 0;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    let stream = Syslog.find({ "tag" : req.params.tag }, null, {
+        sort: { $natural: -1 }
+    }).lean().cursor();
+    res.write("[");
+    stream.on("data", (doc) => {
+        res.write((!(index++) ? "" : ",") + JSON.stringify(doc));
+    });
+    stream.on("close", () => {
+        res.write("]");
+        res.end();
+    });
+    stream.on("error", (err) => {
+        return next(err);
+    });
+});
+
+/**
+ * GET records from Syslog Dataset by hostname
+ */
+router.get("/hostname/:hostname", (req, res, next) => {
+    let index = 0;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    let stream = Syslog.find({ "hostname" : req.params.hostname }, null, {
         sort: { $natural: -1 }
     }).lean().cursor();
     res.write("[");
