883bf4809e7843e60df60cf3b529ba3701c2529d
Lawrence Law <d201.c317@gmail.com>
2017-03-10 07:20:19 +0000

    Improved Error Handling as suggested by ESLint

--- bin/www.js
+++ bin/www.js
@@ -4,22 +4,22 @@
  * Module dependencies.
  */
 
-var app = require("../app");
-var debug = require("debug")("node-apimanager:server");
-var http = require("http");
+const app = require("../app");
+const debug = require("debug")("node-apimanager:server");
+const http = require("http");
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -34,7 +34,7 @@ server.on("listening", onListening);
  */
 
 function normalizePort(val) {
-    var port = parseInt(val, 10);
+    let port = parseInt(val, 10);
 
     if (isNaN(port)) {
         // named pipe
@@ -58,20 +58,16 @@ function onError(error) {
         throw error;
     }
 
-    var bind = typeof port === "string" ?
+    let bind = typeof port === "string" ?
         "Pipe " + port :
         "Port " + port;
 
     // handle specific listen errors with friendly messages
     switch (error.code) {
     case "EACCES":
-        console.error(bind + " requires elevated privileges");
-        process.exit(1);
-        break;
+        throw new Error(bind + " requires elevated privileges");
     case "EADDRINUSE":
-        console.error(bind + " is already in use");
-        process.exit(1);
-        break;
+        throw new Error(bind + " is already in use");
     default:
         throw error;
     }
@@ -82,8 +78,8 @@ function onError(error) {
  */
 
 function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === "string" ?
+    let addr = server.address();
+    let bind = typeof addr === "string" ?
         "pipe " + addr :
         "port " + addr.port;
     debug("Listening on " + bind);
