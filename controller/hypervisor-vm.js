931520f0940123dde2b2e5cf80015a8fb3b24120
Lawrence Law <d201.c317@gmail.com>
2017-03-22 19:20:28 +0000

    Checkpoint: GET Proxy for vm stuffs

--- controller/hypervisor-vm.js
+++ controller/hypervisor-vm.js
@@ -5,14 +5,17 @@ const Express = require('express');
 const Proxy   = require('express-http-proxy');
 const Router = Express.Router();
 
+const URL = require('url');
 
-Router.use('/', Proxy('http://xen-rest/', {
-  forwardPath: '/vm/',
-  limit: '5mb',
-  timeout: (30*1000),
+Router.use('/', Proxy('http://xen-rest:4567/', {
+  forwardPath: (req, res) => {
+    return '/vm' + URL.parse(req.url).path;
+  },
   filter: (req, res) => {
     return req.method === 'GET';
-  }
+  },
+  limit: '5mb',
+  timeout: 30*1000
 }));
 
 Router.post('/create', (req, res, next) => {
