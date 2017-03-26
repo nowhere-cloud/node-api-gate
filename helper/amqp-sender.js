cadd1995b56c4c077d01e0d4b716ad366f12ed77
Lawrence Law <d201.c317@gmail.com>
2017-03-26 23:14:29 +0100

    Integrate Task Storage

--- helper/amqp-sender.js
+++ helper/amqp-sender.js
@@ -29,20 +29,24 @@ class Rabbit {
       sent: false,
       result: {}
     };
+    let task_inst = new Task(msg);
     let target = this.target;
     let promise = new Promise((fulfill, reject) => {
-      amqp.connect(process.env.AMQP_URI, (err, conn) => {
+      task_inst.save((err) => {
         if (err) return reject(err);
-        conn.createChannel((err, ch) => {
+        amqp.connect(process.env.AMQP_URI, (err, conn) => {
           if (err) return reject(err);
-          ch.assertQueue(target, { durable: true });
-          ch.sendToQueue(target, new Buffer.from(JSON.stringify(msg)), {
-            correlationId: msg.uuid
+          conn.createChannel((err, ch) => {
+            if (err) return reject(err);
+            ch.assertQueue(target, { durable: true });
+            ch.sendToQueue(target, new Buffer.from(JSON.stringify(msg)), {
+              correlationId: msg.uuid
+            });
+            ch.close();
           });
-          ch.close();
+          conn.close();
+          return fulfill(msg.uuid);
         });
-        conn.close();
-        return fulfill(msg.uuid);
       });
     });
     return promise;
