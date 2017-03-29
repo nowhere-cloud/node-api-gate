b5fd235a8a9b7cd309ce1734bccf8d94d91d2370
Lawrence Law <d201.c317@gmail.com>
2017-03-29 05:23:42 +0100

    fix typo

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    usernamee: DataTypes.STRING,
+    username: DataTypes.STRING,
     hash: DataTypes.STRING,
     salt: DataTypes.STRING
   }, {
