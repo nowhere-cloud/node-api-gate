08901332722b85e57f8a7c974c53d67aa9d6960f
Lawrence Law <d201.c317@gmail.com>
2017-05-07 14:03:31 +0100

    Use VM-Native Storage

--- models/user.js
+++ models/user.js
@@ -7,7 +7,6 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        User.hasMany(models.vm_user);
         User.hasMany(models.dns_record);
       }
     }
